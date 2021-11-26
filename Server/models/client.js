af969477c6910c78c9260f0e7610d796aa1e7c44
OmarQuezada13 <omar.wst13@gmail.com>
2021-11-26 14:43:37 -0600

    Renta

--- Server/models/client.js
+++ Server/models/client.js
@@ -3,12 +3,9 @@ const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let clientSchema = new Schema({
-<<<<<<< HEAD
     shortId: {
         type: String
     },
-=======
->>>>>>> cab2374dbcacea6172204f5f0d6882dbcb164240
     nombre: {
         type: String
     },
@@ -20,12 +17,10 @@ let clientSchema = new Schema({
     },
     direccion: {
         type: String
-<<<<<<< HEAD
     },
     correo: {
         type: String
-=======
->>>>>>> cab2374dbcacea6172204f5f0d6882dbcb164240
+
     }
 });
 
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -6,11 +6,6 @@ app.use(require('./libro'));
 app.use(require('./renta'));
 app.use(require('./login'));
 app.use(require('./client'));
-<<<<<<< HEAD
-
-=======
->>>>>>> cab2374dbcacea6172204f5f0d6882dbcb164240
-
 
 
 module.exports = app;
